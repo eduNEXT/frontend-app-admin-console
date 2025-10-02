08740d4bc793d0ca8848bb91b33601d4779de680
Diana Olarte <diana.olarte@edunext.co>
2025-10-02 23:09:41 +1000

    feat: add roles tab
    refactor: add loading state to the roles cards

--- src/authz-module/libraries-manager/LibrariesTeamManager.tsx
+++ src/authz-module/libraries-manager/LibrariesTeamManager.tsx
@@ -1,10 +1,15 @@
+import { useMemo } from 'react';
 import { useIntl } from '@edx/frontend-platform/i18n';
-import { Tab, Tabs } from '@openedx/paragon';
+import {
+  Container, Skeleton, Tab, Tabs,
+} from '@openedx/paragon';
 import { useLibrary } from '@src/authz-module/data/hooks';
 import TeamTable from './components/TeamTable';
 import AuthZLayout from '../components/AuthZLayout';
 import { AddNewTeamMemberTrigger } from './components/AddNewTeamMemberModal';
+import RoleCard from '../components/RoleCard';
 import { useLibraryAuthZ } from './context';
+import { buildPermissionsByRoleMatrix } from './utils';
 
 import messages from './messages';
 import { useLocation } from 'react-router-dom';
@@ -12,10 +17,18 @@ import { useLocation } from 'react-router-dom';
 const LibrariesTeamManager = () => {
   const intl = useIntl();
   const location = useLocation();
-  const hash = location.hash; const { libraryId, canManageTeam } = useLibraryAuthZ();
+  const hash = location.hash;
+  const { libraryId, canManageTeam, permissions, resources, roles, } = useLibraryAuthZ();
   const { data: library } = useLibrary(libraryId);
   const rootBradecrumb = intl.formatMessage(messages['library.authz.breadcrumb.root']) || '';
   const pageTitle = intl.formatMessage(messages['library.authz.manage.page.title']);
+  const libraryRoles = useMemo(() => roles.map(role => ({
+    ...role,
+    permissions: buildPermissionsByRoleMatrix({
+      rolePermissions: role.permissions, permissions, resources, intl,
+    }),
+  })), [roles, permissions, resources, intl]);
+
   return (
     <div className="authz-libraries">
       <AuthZLayout
@@ -32,14 +45,25 @@ const LibrariesTeamManager = () => {
       >
         <Tabs
           variant="tabs"
-          defaultActiveKey={ hash ? 'permissions' : 'team'}
+          defaultActiveKey={hash ? 'permissions' : 'team'}
           className="bg-light-100 px-5"
         >
           <Tab eventKey="team" title={intl.formatMessage(messages['library.authz.tabs.team'])} className="p-5">
             <TeamTable />
           </Tab>
           <Tab eventKey="roles" title={intl.formatMessage(messages['library.authz.tabs.roles'])}>
-            Role tab.
+            <Container className="p-5">
+              {!libraryRoles ? <Skeleton count={2} height={200} /> : null}
+              {libraryRoles && libraryRoles.map(role => (
+                <RoleCard
+                  key={`${role}-description`}
+                  title={role.name}
+                  userCounter={role.userCount}
+                  description={role.description}
+                  permissions={role.permissions as any[]}
+                />
+              ))}
+            </Container>
           </Tab>
           <Tab id="libraries-permissions-tab" eventKey="permissions" title={intl.formatMessage(messages['library.authz.tabs.permissions'])}>
             Permissions tab.
