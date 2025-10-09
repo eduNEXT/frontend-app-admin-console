7e52102b18b8d7537592b8508114eb686f047637
Brayan Ceron <brayan.ceron@edunext.co>
2025-10-09 16:48:32 -0500

    feat: add ConfirmDeletionModal for role removal confirmation in LibrariesUserManager

--- src/authz-module/libraries-manager/LibrariesUserManager.tsx
+++ src/authz-module/libraries-manager/LibrariesUserManager.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { Container } from '@openedx/paragon';
 import { ROUTES } from '@src/authz-module/constants';
+import { Role } from 'types';
 import AuthZLayout from '../components/AuthZLayout';
 import { useLibraryAuthZ } from './context';
 import RoleCard from '../components/RoleCard';
@@ -11,6 +12,7 @@ import { buildPermissionMatrixByRole } from './utils';
 import { useLibrary, useRevokeUserRoles, useTeamMembers } from '../data/hooks';
 
 import messages from './messages';
+import ConfirmDeletionModal from './components/ConfirmDeletionModal';
 
 const LibrariesUserManager = () => {
   const intl = useIntl();
@@ -22,7 +24,7 @@ const LibrariesUserManager = () => {
     libraryId, permissions, roles, resources, canManageTeam,
   } = useLibraryAuthZ();
   const { data: library } = useLibrary(libraryId);
-  const { mutate: revokeUserRoles } = useRevokeUserRoles();
+  const { mutate: revokeUserRoles, isPending: isRevokingUserRole } = useRevokeUserRoles();
   const rootBreadcrumb = intl.formatMessage(messages['library.authz.breadcrumb.root']) || '';
   const pageManageTitle = intl.formatMessage(messages['library.authz.manage.page.title']);
   const teamMembersPath = `/authz/${ROUTES.LIBRARIES_TEAM_PATH.replace(':libraryId', libraryId)}`;
@@ -32,6 +34,9 @@ const LibrariesUserManager = () => {
   //const user = teamMembers?.find(member => member.username === username);
   const user = location?.state?.user;
   console.log(user)
+  const [roleToDelete, setRoleToDelete] = useState('');
+  const [showConfirmDeletionModal, setShowConfirmDeletionModal] = useState(false);
+
   const userRoles = useMemo(() => {
     const assignedRoles = roles.filter(role => user?.roles.includes(role.role));
     return buildPermissionMatrixByRole({
@@ -39,15 +44,29 @@ const LibrariesUserManager = () => {
     });
   }, [roles, user?.roles, permissions, resources, intl]);
 
+  const handleCloseConfirmDeletionModal = () => {
+    setShowConfirmDeletionModal(false);
+    setRoleToDelete('');
+  };
+
+  const handleShowConfirmDeletionModal = (role: Pick<Role, 'name' | 'role'>) => {
+    setRoleToDelete(role.name);
+    setShowConfirmDeletionModal(true);
+  };
+
   const handleRevokeUserRole = (role: string) => {
     if (user) {
       const data = {
         users: user.username,
-        role,
+        role: roles.find(r => r.name === role)!.role,
         scope: libraryId,
       };
 
-      revokeUserRoles({ data });
+      revokeUserRoles({ data }, {
+        onSuccess: () => {
+          handleCloseConfirmDeletionModal();
+        },
+      });
     }
   };
 
@@ -60,6 +79,19 @@ const LibrariesUserManager = () => {
 
   return (
     <div className="authz-libraries">
+      <ConfirmDeletionModal
+        isOpen={showConfirmDeletionModal}
+        close={handleCloseConfirmDeletionModal}
+        onSave={() => handleRevokeUserRole(roleToDelete)}
+        isDeleting={isRevokingUserRole}
+        context={{
+          userName: user?.username || '',
+          scope: library.title,
+          role: roleToDelete,
+          rolesCount: userRoles.length,
+        }}
+      />
+
       <AuthZLayout
         context={{ id: libraryId, title: library.title, org: library.org }}
         navLinks={[{ label: rootBreadcrumb }, { label: pageManageTitle, to: teamMembersPath }]}
@@ -82,7 +114,7 @@ const LibrariesUserManager = () => {
               objectName={library.title}
               description={role.description}
               permissionsByResource={role.resources as any[]}
-              handleDelete={() => handleRevokeUserRole(role.role)}
+              handleDelete={() => handleShowConfirmDeletionModal(role)}
             />
           ))}
         </Container>
